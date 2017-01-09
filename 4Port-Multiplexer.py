a05ebb7f106df874701d19d5232a3d6398790bcc
telliottoscoela <travis"controlanything.com>
2017-01-09 19:35:16 +0000

    initial commit

---
+++ 4Port-Multiplexer.py
@@ -0,0 +1,29 @@
+import smbus
+import time
+import sys
+
+address = 113
+
+
+bus = smbus.SMBus(1)
+print "start"
+
+i = 1
+port = 1
+while(i < 9):
+    present = False
+    bus.write_byte(address, i)
+    time.sleep(0.1)
+    print 'port %i' % port
+    for x in range(1,127):
+        try:
+            status = bus.write_quick(x)
+            if(x != 113):
+               print 'device present at %i' % x
+               present = True
+        except IOError, err:
+            continue
+    if(present != True):
+        print 'no devices on port %i' % port
+    port = port + 1
+    i = i * 2
